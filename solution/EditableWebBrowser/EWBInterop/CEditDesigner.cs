9d83e2ae771acc3f1121534a27dda1be094abe1f
artmachinez <hvlasak@volny.cz>
2011-04-04 00:18:40 +0200

    editdesigner

---
+++ solution/EditableWebBrowser/EWBInterop/CEditDesigner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EditableWebBrowser.EWBInterop
+{
+    class CEditDesigner : EWBInterop.IHTMLEditDesigner
+    {
+
+        #region IHTMLEditDesigner Members
+
+        public void PostEditorEventNotify(int disp, mshtml.IHTMLEventObj eventObj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TranslateAccelerator(int disp, mshtml.IHTMLEventObj eventObj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PostHandleEvent(int disp, mshtml.IHTMLEventObj eventObj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PreHandleEvent(int disp, mshtml.IHTMLEventObj eventObj)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
